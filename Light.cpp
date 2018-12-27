dc05ebf4e12660480d590bf98fbf6b3ff7f9cba4
Dmitry Novikov <nerosketch@gmail.com>
2018-12-27 03:42:09 +0300

    Начал добавлятьбольше одного источника освещения

--- Light.cpp
+++ Light.cpp
@@ -5,50 +5,69 @@
  * Created on December 25, 2018, 1:22 AM
  */
 
+#include <vector>
+#include <string>
 #include "Light.h"
 #include "resources.h"
 
 
+using namespace std;
+
+#define ADDR_BUF_LEN 32
 
 class CustomUniformMat : public STDMaterial
 {
+private:
+    char _addr_buf[ADDR_BUF_LEN];
 public:
     MATX(CustomUniformMat);
 
-    Vector4 light_pos_radius_intense = Vector4(455.f, 100.f, 200.f, 1.f);
-    Vector4 light_color = Vector4(1.f, 1.f, 1.f, 1.f);
-    float ambient_intense = 1.f;
-
-    /*void rehash(size_t& hash) const override
-    {
-        logs::messageln("rehash");
-        STDMaterial::rehash(hash);
-        hash_combine(hash, light_pos.x, light_pos.y);
-    }*/
+    vector<LightPoint> _lights;
+    uint lights_count = 0;
+    float _ambient_intense=1.f;
 
     static bool cmp(const CustomUniformMat& a, const CustomUniformMat& b)
     {
         if (!STDMaterial::cmp(a, b))
             return false;
 
-        return a.light_pos_radius_intense == b.light_pos_radius_intense &&
+        /*return a.light_pos_radius_intense == b.light_pos_radius_intense &&
                 a.light_color == b.light_color &&
-                a.ambient_intense == b.ambient_intense;
+                a.ambient_intense == b.ambient_intense;*/
+        return false;
     }
 
     void xapply() override
     {
         STDMaterial::xapply();
-        IVideoDriver::instance->setUniform("light_pos_radius_intense", light_pos_radius_intense);
-        IVideoDriver::instance->setUniform("light_color", light_color);
-        IVideoDriver::instance->setUniform("ambient_intense", ambient_intense);
+
+        IVideoDriver::instance->setUniformInt("lights_count", lights_count);
+        IVideoDriver::instance->setUniform("ambient_intense", _ambient_intense);
+        uint n = 0;
+        for(const LightPoint& l : _lights)
+        {
+            if(n > 15)
+                break;
+
+            snprintf(_addr_buf, ADDR_BUF_LEN, "lights[%d].light_pos", n);
+            IVideoDriver::instance->setUniform(_addr_buf, l);
+
+            snprintf(_addr_buf, ADDR_BUF_LEN, "lights[%d].light_color", n);
+            IVideoDriver::instance->setUniform(_addr_buf, l._light_color);
+
+            snprintf(_addr_buf, ADDR_BUF_LEN, "lights[%d].radius", n);
+            IVideoDriver::instance->setUniform(_addr_buf, l._radius);
+
+            snprintf(_addr_buf, ADDR_BUF_LEN, "lights[%d].intense", n);
+            IVideoDriver::instance->setUniform(_addr_buf, l._intense);
+
+            n++;
+        }
     }
 };
 
 
-
-
-
+// #################### Light Start #####################
 
 
 Light::Light() : _shaderLight(0)
@@ -56,30 +75,71 @@ Light::Light() : _shaderLight(0)
     _shaderLight = new UberShaderProgram;
     _shaderLight->init(STDRenderer::uberShaderBody, R"(
         #define MODIFY_BASE
-        uniform lowp vec4 light_pos_radius_intense;
-        uniform lowp vec4 light_color;
+        struct LIGHT_INFO{
+            vec4 light_color;
+            vec2 light_pos;
+            float radius;
+            float intense;
+        };
+        uniform lowp int lights_count;
         uniform lowp float ambient_intense;
+        uniform lowp LIGHT_INFO lights[16];
+
         lowp vec4 modify_base(lowp vec4 base)
         {
-            base = base * light_color;
-            float radius = light_pos_radius_intense.z;
-            float dist = distance(light_pos_radius_intense.xy, gl_FragCoord.xy);
-            if(dist < radius)
+
+            for(int i = 0; i < lights_count; ++i)
             {
-                float k = 1.0 + ambient_intense - (dist / radius);
-                return base * k * light_pos_radius_intense.a;
-            }else
-                return base * ambient_intense * light_pos_radius_intense.a;
+                base *= lights[i].light_color;
+                float dist = distance(lights[i].light_pos, gl_FragCoord.xy);
+                if(dist < lights[i].radius)
+                {
+                    float k = 1.0 - (dist / lights[i].radius);
+                    base *= lights[i].intense * k;
+                }else
+                {
+                    base *= lights[i].intense;
+                }
+            }
+            return base * ambient_intense;
         }
     )");
+
+    /*
+     * Часть успешного шейдера
+    base = base * light2.light_color;
+    dist = distance(lights[n].light_pos, gl_FragCoord.xy);
+    if(dist < lights[n].radius)
+    {
+        k += 1.0 + lights[n].ambient_intense - (dist / lights[n].radius);
+        base = base * k * light2.intense;
+    }else
+        base = base * light2.ambient_intense * light2.intense;
+    */
 
     CustomUniformMat mat;
     _mat->copyTo(mat);
 
     mat._uberShader = _shaderLight;
+    mat._lights.resize(2);
+    mat._lights[0] = LightPoint(555.f, 100.f);
+    
+    Vector4 light_color(0.5f, 0.5f, 0.9f, 0.0f);
+    mat._lights[0].set_light_color(light_color);
+    mat._lights[0].set_intense(3.8f);
+    mat._lights[0].set_radius(40.f);
+
+    mat._lights[1] = LightPoint(255.f, 100.f);
+    light_color = Vector4(0.9f, 0.5f, 0.5f, 0.0f);
+    mat._lights[1].set_light_color(light_color);
+    mat._lights[1].set_intense(1.8f);
+
+    set_ambient_intense(0.4f);
+
+    mat.lights_count = mat._lights.size();
+
     _mat = mc().cache(mat);
 
-    //setResAnim(res::resources.getResAnim("panel"));
 }
 
 Light::Light(const Light&)
@@ -92,78 +152,55 @@ Light::~Light()
 }
 
 
-void Light::set_light_pos(const float x, const float y)
+void Light::add_light(const LightPoint& p)
 {
     CustomUniformMat *p_mat = static_cast<CustomUniformMat*>(_mat.get());
-    Vector4& lp = p_mat->light_pos_radius_intense;
-    lp.x = x;
-    lp.y = y;
+    p_mat->_lights.push_back(p);
+    p_mat->lights_count = p_mat->_lights.size();
 }
 
+void Light::remove_light(LightPoint& p)
+{
+    //CustomUniformMat *p_mat = static_cast<STDMaterial*>(_mat.get());
+    //p_mat->_lights.push_back(p);
+}
 
-const Vector2 Light::get_light_pos() const
+void Light::set_ambient_intense(const float ai)
 {
     CustomUniformMat *p_mat = static_cast<CustomUniformMat*>(_mat.get());
-    const Vector4& lp = p_mat->light_pos_radius_intense;
-    return Vector2(lp.x, lp.y);
+    p_mat->_ambient_intense = ai;
 }
-
-
-void Light::set_radius(const float radius)
-{
-    CustomUniformMat *p_mat = static_cast<CustomUniformMat*>(_mat.get());
-    Vector4& lp = p_mat->light_pos_radius_intense;
-    lp.z = radius;
-}
-
-
-float Light::get_radius() const
-{
-    CustomUniformMat *p_mat = static_cast<CustomUniformMat*>(_mat.get());
-    const Vector4& lp = p_mat->light_pos_radius_intense;
-    return lp.z;
-}
-
-
-void Light::set_intense(const float intense)
-{
-    CustomUniformMat *p_mat = static_cast<CustomUniformMat*>(_mat.get());
-    Vector4& lp = p_mat->light_pos_radius_intense;
-    lp.w = intense;
-}
-
-
-float Light::get_intense() const
-{
-    CustomUniformMat *p_mat = static_cast<CustomUniformMat*>(_mat.get());
-    const Vector4& lp = p_mat->light_pos_radius_intense;
-    return lp.w;
-}
-
-
-void Light::set_light_color(const Vector4& c)
-{
-    CustomUniformMat *p_mat = static_cast<CustomUniformMat*>(_mat.get());
-    p_mat->light_color = c;
-}
-
-
-const Vector4& Light::get_light_color() const
-{
-    CustomUniformMat *p_mat = static_cast<CustomUniformMat*>(_mat.get());
-    return p_mat->light_color;
-}
-
-
-void Light::set_ambient_intense(const float ambient_intense)
-{
-    CustomUniformMat *p_mat = static_cast<CustomUniformMat*>(_mat.get());
-    p_mat->ambient_intense = ambient_intense;
-}
-
 
 float Light::get_ambient_intense() const
 {
     CustomUniformMat *p_mat = static_cast<CustomUniformMat*>(_mat.get());
-    return p_mat->ambient_intense;
+    return p_mat->_ambient_intense;
 }
+
+
+// #################### Light End #####################
+
+
+// #################### LightPoint Start #####################
+
+
+LightPoint::LightPoint() : _light_color(1.f, 1.f, 1.f, 1.f),
+        _radius(200.f), _intense(1.f)
+{}
+
+LightPoint::LightPoint(float x, float y) : Vector2(x, y),
+        _light_color(1.f, 1.f, 1.f, 1.f),
+        _radius(200.f), _intense(1.f)
+{
+    set(x, y);
+}
+
+LightPoint::LightPoint(const LightPoint& o) :
+        Vector2(o), _light_color(o._light_color),
+        _radius(o._radius), _intense(o._intense)
+{}
+
+LightPoint::~LightPoint()
+{}
+
+// #################### LightPoint End #####################
--- Light.h
+++ Light.h
@@ -5,6 +5,7 @@
  * Created on December 25, 2018, 1:22 AM
  */
 
+#include "flags.h"
 #include <oxygine-framework.h>
 
 
@@ -18,6 +19,51 @@ using namespace oxygine;
 DECLARE_SMART(Light, spLight);
 
 
+class LightPoint : public Vector2
+{
+private:
+    friend class CustomUniformMat;
+    Vector4 _light_color;
+    //Vector2 _light_pos;
+    float _radius;
+    float _intense;
+
+public:
+    LightPoint();
+    LightPoint(float x, float y);
+    LightPoint(const LightPoint&);
+    virtual ~LightPoint();
+
+    inline void set_radius(const float radius)
+    {
+        _radius = radius;
+    }
+    inline float get_radius() const
+    {
+        return _radius;
+    }
+
+    inline void set_intense(const float intense)
+    {
+        _intense = intense;
+    }
+    inline float get_intense() const
+    {
+        return _intense;
+    }
+
+    inline void set_light_color(const Vector4& c)
+    {
+        _light_color = c;
+    }
+    inline const Vector4& get_light_color() const
+    {
+        return _light_color;
+    }
+
+};
+
+
 class Light :public Sprite
 {
 private:
@@ -29,19 +75,10 @@ public:
     Light(const Light& orig);
     virtual ~Light();
 
-    void set_light_pos(const float x, const float y);
-    const Vector2 get_light_pos() const;
+    void add_light(const LightPoint&);
+    void remove_light(LightPoint&);
 
-    void set_radius(const float radius);
-    float get_radius() const;
-
-    void set_intense(const float intense);
-    float get_intense() const;
-
-    void set_light_color(const Vector4&);
-    const Vector4& get_light_color() const;
-
-    void set_ambient_intense(const float ambient_intense);
+    void set_ambient_intense(const float);
     float get_ambient_intense() const;
 
 };
